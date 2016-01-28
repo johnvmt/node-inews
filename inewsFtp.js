45e50a9de6b8d7e7b662bd39e6f509681b4819b7
John Murphy-Teixidor <johnvmt@gmail.com>
2016-01-28 01:04:50 -0500

    Working state, non-tested

---
+++ inewsFtp.js
@@ -0,0 +1,110 @@
+module.exports = function(config) {
+	return new InewsClient(config);
+};
+
+var FtpClient = require('ftp');
+var fs = require('fs');
+var parseNsml = require('./inewsStoryParser');
+
+function InewsClient(config) {
+	this.config = config;
+	this._ftpConn = new FtpClient();
+}
+
+InewsClient.prototype.connect = function(callback) {
+	if(this._ftpConn.connected)
+		callback();
+	else {
+		this._ftpConn.on('ready', callback);
+		this._ftpConn.connect(this.config);
+	}
+};
+
+InewsClient.prototype.list = function(callback) {
+	var self = this;
+	var ftpConn = this._ftpConn;
+	this.connect(function() {
+		ftpConn.list(function(error, list) {
+			if(error)
+				callback(error, null);
+			else {
+				var fileNames = [];
+
+				list.forEach(function(listItem) {
+					if(self._listItemIsFile(listItem)) {
+						var file = self._filenameFromListItem(listItem);
+						if(typeof file !== 'undefined')
+							fileNames.push({type: 'file', file: file});
+					}
+					else if(self._listItemIsQueue(listItem)) {
+						var file = self._queueFromListItem(listItem);
+						if (typeof file !== 'undefined')
+							fileNames.push({type: 'queue', file: file});
+					}
+				});
+				callback(null, fileNames);
+			}
+		});
+	});
+};
+
+InewsClient.prototype._listItemIsQueue = function(listItem) {
+	return listItem.indexOf('d---------') === 0;
+};
+
+InewsClient.prototype._queueFromListItem = function(listItem) {
+	var pattern = /.([A-Za-z0-9]*)$/;
+	var matchParts = listItem.match(pattern);
+	return matchParts === null ? undefined : matchParts[1];
+};
+
+InewsClient.prototype._listItemIsFile = function(listItem) {
+	return this._filenameFromListItem(listItem) !== undefined;
+};
+
+InewsClient.prototype._filenameFromListItem = function(listItem) {
+	var pattern = /[A-Z0-9]{8}:[A-Z0-9]{8}:[A-Z0-9]{8}/i;
+	var matchParts = listItem.match(pattern);
+	return matchParts === null ? undefined : matchParts[0];
+};
+
+InewsClient.prototype._parseNsml = function(nsml, callback) {
+	parseNsml(nsml, callback);
+};
+
+InewsClient.prototype.getStory = function(file, callback) {
+	var self = this;
+	this.get(file, function(error, storyXml) {
+		if(error)
+			callback(error, null);
+		else
+			self._parseNsml(storyXml, callback);
+	});
+};
+
+InewsClient.prototype.get = function(file, callback) {
+	var ftpConn = this._ftpConn;
+	this.connect(function() {
+		ftpConn.get(file, function(error, stream) {
+			if(error)
+				callback(error, null);
+			else {
+				var storyXml = "";
+				stream.setEncoding('utf8');
+				stream.on('data', function(chunk) {
+					storyXml += chunk;
+				});
+				stream.once('close', function() {
+					callback(null, storyXml);
+				});
+			}
+		});
+	});
+};
+
+InewsClient.prototype.cwd = function(path, callback) {
+	var ftpConn = this._ftpConn;
+	this.connect(function() {
+		ftpConn.cwd(path, callback);
+	});
+};
---
+++ inewsStoryParser.js
@@ -0,0 +1,116 @@
+module.exports = function(nsml, callback) {
+	var htmlparser = require("htmlparser");
+
+	var parseHandler = new htmlparser.DefaultHandler(function (error, dom) {
+		if (error)
+			callback(error, null);
+		else
+			callback(null, parseNsml(dom));
+	});
+
+	var parser = new htmlparser.Parser(parseHandler);
+	parser.parseComplete(nsml);
+
+	function parseNsml(nodes, story) {
+		if(typeof story === 'undefined') {
+			story = {
+				fields: {},
+				cues: []
+			};
+		}
+
+		nodes.forEach(function(node) {
+			parseNsmlNode(node, story);
+		});
+
+		return story;
+	}
+
+	function stringifyNodes(nodes) {
+		var nodeStr = "";
+		if(Array.isArray(nodes)) {
+			nodes.forEach(function (node) {
+				nodeStr += stringifyNode(node);
+			});
+		}
+		return nodeStr;
+	}
+
+	function stringifyNode(node) {
+		if(node.type === 'text')
+			return node.data;
+		else if(node.type === 'tag') {
+			var nodeStr = "<" + node.name;
+			var attrStr = stringifyAttributes(node.attribs);
+			if(attrStr.length > 0)
+				nodeStr += " " + attrStr;
+			nodeStr += ">";
+			nodeStr += stringifyNodes(node.children);
+			nodeStr += "</" + node.name + ">";
+		}
+
+		return nodeStr;
+	}
+
+	function stringifyAttributes(attributes) {
+		var attrStr = "";
+		for (var key in attributes) {
+			if(attrStr.length > 0)
+				attrStr += " ";
+			attrStr += key + "=\"" + attributes[key].replace(/\"/g,'\\"') + "\"";
+		}
+		return attrStr;
+	}
+
+	function nodesToArray(nodes, tag) {
+		var lines = [];
+
+		nodes.forEach(function(node) {
+			if(node.type === 'tag') {
+				//console.log(node);
+				if(node.name === tag)
+					lines.push(stringifyNodes(node.children));
+//				else
+					lines = lines.concat(nodesToArray(node.children, tag));
+			}
+		});
+
+		return lines;
+	}
+
+	function parseNsmlNode(node, story) {
+		if(node.type === 'tag') {
+			switch(node.name) {
+				case 'ae':
+					try {
+						var id = node.attribs['id'];
+						story.cues[id] = nodesToArray(node.children, 'ap');
+					}
+					catch(error) {}
+					break;
+				case 'body':
+					story.body = stringifyNodes(node.children);
+					break;
+				case 'storyid':
+					try {
+						story.id = node.children[0]['data'];
+					}
+					catch(error) {}
+					break;
+				case 'f':
+					try {
+						var key = node.attribs['id'];
+						var val = node.children[0]['data'];
+						story.fields[key] = val;
+					}
+					catch(error) {}
+					break;
+				default:
+					if(Array.isArray(node.children))
+						parseNsml(node.children, story);
+
+					break;
+			}
+		}
+	}
+};
