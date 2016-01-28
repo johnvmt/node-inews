6c3fe9411175a5f6b896276f3e0bba5979f7f024
johnvmt <johnvmt@gmail.com>
2016-01-28 12:21:22 -0500

    Added date to file list

--- inewsFtp.js
+++ inewsFtp.js
@@ -19,6 +19,13 @@ InewsClient.prototype.connect = function(callback) {
 	}
 };
 
+InewsClient.prototype.cwd = function(path, callback) {
+	var ftpConn = this._ftpConn;
+	this.connect(function() {
+		ftpConn.cwd(path, callback);
+	});
+};
+
 InewsClient.prototype.list = function(callback) {
 	var self = this;
 	var ftpConn = this._ftpConn;
@@ -28,56 +35,14 @@ InewsClient.prototype.list = function(callback) {
 				callback(error, null);
 			else {
 				var fileNames = [];
-
 				list.forEach(function(listItem) {
-					if(self._listItemIsFile(listItem)) {
-						var file = self._filenameFromListItem(listItem);
-						if(typeof file !== 'undefined')
-							fileNames.push({type: 'file', file: file});
-					}
-					else if(self._listItemIsQueue(listItem)) {
-						var file = self._queueFromListItem(listItem);
-						if (typeof file !== 'undefined')
-							fileNames.push({type: 'queue', file: file});
-					}
+					var file = self._fileFromListItem(listItem);
+					if(typeof file !== 'undefined')
+						fileNames.push(file);
 				});
 				callback(null, fileNames);
 			}
 		});
-	});
-};
-
-InewsClient.prototype._listItemIsQueue = function(listItem) {
-	return listItem.indexOf('d---------') === 0;
-};
-
-InewsClient.prototype._queueFromListItem = function(listItem) {
-	var pattern = /.([A-Za-z0-9]*)$/;
-	var matchParts = listItem.match(pattern);
-	return matchParts === null ? undefined : matchParts[1];
-};
-
-InewsClient.prototype._listItemIsFile = function(listItem) {
-	return this._filenameFromListItem(listItem) !== undefined;
-};
-
-InewsClient.prototype._filenameFromListItem = function(listItem) {
-	var pattern = /[A-Z0-9]{8}:[A-Z0-9]{8}:[A-Z0-9]{8}/i;
-	var matchParts = listItem.match(pattern);
-	return matchParts === null ? undefined : matchParts[0];
-};
-
-InewsClient.prototype._parseNsml = function(nsml, callback) {
-	parseNsml(nsml, callback);
-};
-
-InewsClient.prototype.getStory = function(file, callback) {
-	var self = this;
-	this.get(file, function(error, storyXml) {
-		if(error)
-			callback(error, null);
-		else
-			self._parseNsml(storyXml, callback);
 	});
 };
 
@@ -101,9 +66,75 @@ InewsClient.prototype.get = function(file, callback) {
 	});
 };
 
-InewsClient.prototype.cwd = function(path, callback) {
-	var ftpConn = this._ftpConn;
-	this.connect(function() {
-		ftpConn.cwd(path, callback);
+InewsClient.prototype.getStory = function(file, callback) {
+	var self = this;
+	this.get(file, function(error, storyXml) {
+		if(error)
+			callback(error, null);
+		else
+			self.parseNsml(storyXml, callback);
 	});
+};
+
+InewsClient.prototype.parseNsml = function(nsml, callback) {
+	parseNsml(nsml, callback);
+};
+
+InewsClient.prototype._fileFromListItem = function(listItem) {
+	if(this._listItemIsFile(listItem)) {
+		var fileName = this._filenameFromListItem(listItem);
+		if(typeof fileName !== 'undefined')
+			var file = {type: 'file', file: fileName};
+	}
+	else if(this._listItemIsQueue(listItem)) {
+		var fileName = this._queueFromListItem(listItem);
+		if(typeof fileName !== 'undefined')
+			var file = {type: 'queue', file: fileName};
+	}
+
+	if(typeof file !== 'undefined') {
+		var fileDate = this._dateFromListItem(listItem);
+		if(typeof fileDate !== 'undefined')
+			file['modified'] = fileDate;
+		return file;
+	}
+	else
+		return undefined;
+};
+
+InewsClient.prototype._dateFromListItem = function(listItem) {
+	var pattern = / ([A-Za-z]{3,4})[ ]+([0-9]{1,2})[ ]+([0-9]{4}|([0-9]{1,2}):([0-9]{2}))/i;
+	var dateParts = listItem.match(pattern);
+
+	try {
+		if(typeof dateParts[4] !== 'undefined')
+			var dateStr = dateParts[1] + " " + dateParts[2] + " " + new Date().getFullYear() + " " + dateParts[3];
+		else
+			var dateStr = dateParts[0];
+
+		return new Date(dateStr);
+	}
+	catch(error) {
+		return undefined;
+	}
+};
+
+InewsClient.prototype._listItemIsQueue = function(listItem) {
+	return listItem.indexOf('d---------') === 0;
+};
+
+InewsClient.prototype._queueFromListItem = function(listItem) {
+	var pattern = /.([A-Za-z0-9]*)$/;
+	var matchParts = listItem.match(pattern);
+	return matchParts === null ? undefined : matchParts[1];
+};
+
+InewsClient.prototype._listItemIsFile = function(listItem) {
+	return this._filenameFromListItem(listItem) !== undefined;
+};
+
+InewsClient.prototype._filenameFromListItem = function(listItem) {
+	var pattern = /[A-Z0-9]{8}:[A-Z0-9]{8}:[A-Z0-9]{8}/i;
+	var matchParts = listItem.match(pattern);
+	return matchParts === null ? undefined : matchParts[0];
 };
